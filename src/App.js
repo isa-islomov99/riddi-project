09c274175adf192836db6d482ebe09da6670fe11
gulhayoarabova <gulhayoarabova@gmail.com>
2021-05-03 18:29:36 +0500

    new release

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import MainPage from './components/mainPage/MainPage';
 import OurShirts from './components/ourShirts/OurShirts'
 import Shop from './components/shop/Shop';
 import ShopItem from './components/shopItem/ShopItem';
+import AboutPage from './components/about/aboutPage';
+import HowToSelectCustomShirt from './components/how-to-select-custom-shirt/how-to-select-custom-shirt';
 
 function App() {
   return (
@@ -14,6 +16,10 @@ function App() {
         <Route path="/ourShirts" component={OurShirts} />
         <Route path="/shop" exact component={Shop} />
         <Route path="/shopItem" exact component={ShopItem}/>
+        <Route path="/about" exact component={AboutPage}/>
+        <Route path="/shopItem" exact component={ShopItem}/>
+        <Route path="/selectCustomShirt" exact component={HowToSelectCustomShirt}/>
+
       </Switch>
       <Footer />
     </div>
